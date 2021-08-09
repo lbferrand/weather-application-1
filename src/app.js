20efda87155c83509d9ffa2aef5289e48bb0f232
lbferrand <lbferrand@gmail.com>
2021-08-08 22:23:55 -0400

    Update app.js with city variable

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "4e6401358feb8a28e9ae1c46c2f907bc";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
